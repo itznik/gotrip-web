effe6f49092b686ffe82874b8874f3aec572de22
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2025-12-26 22:34:26 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-brand-light">
+    // Changed bg-brand-light to bg-brand-dark
+    <main className="min-h-screen bg-brand-dark">
       <Navbar />
       <Hero />
       
-      {/* Temporary spacing to see the scrolling effect */}
+      {/* Temporary spacing */}
       <div className="h-[500px] flex items-center justify-center text-gray-400">
         Next Section: Featured Destinations (Coming Soon)
       </div>
